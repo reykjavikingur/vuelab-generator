c40a06249f160e2887c532a1f52b8a9a9e454fb3
Andrew Strader <astrader@resourceammirati.com>
2017-10-01 23:27:17 -0400

    revise messaging

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const package = require('./package');
+#!/usr/bin/env node
+
+const pkg = require('./package');
 const Path = require('path');
 const gnat = require('gnat');
+const Promise = require('promise');
+const child_process = require('child_process');
 
 function main(args) {
     var command = args.shift();
@@ -14,19 +18,45 @@ function main(args) {
             process.exit(1);
         }
         else {
+            console.log('');
+            console.log('Cloning into directory:', path);
+            console.log('Please answer the following questions about your project (press enter to use the default):');
             gnat.clone('https://github.com/reykjavikingur/vuelab.git', path)
                 .then(r => {
-                    console.log('Initialization complete');
-                    process.exit(0);
+                    console.log('');
+                    console.log('vuelab clone complete');
+                    console.log('Changing directory:', path);
+                    process.chdir(path);
                 }, e => {
-                    console.error('Initialization failed: ', e);
+                    console.error('vuelab clone failed', e);
                     process.exit(1);
+                })
+                .then(r => {
+                    return exec('npm install');
+                })
+                .then(r => {
+                    return exec('git init');
+                })
+                .then(r => {
+                    return exec('git add .');
+                })
+                .then(r => {
+                    return exec('git commit -m "Initialize vuelab project"');
+                })
+                .then(r => {
+                    console.log('vuelab initialization complete');
+                    console.log('Now run the following commands to get started:');
+                    console.log(`cd "${path}"`);
+                    console.log('npm start');
+                }, e => {
+                    console.error('Error: vuelab initialization failed', e);
                 })
             ;
         }
     }
     else if (command == 'create') {
-
+        Array.prototype.splice.call(process.argv, 2, 1);
+        require(Path.join(process.cwd(), 'bin/create'));
     }
     else {
         console.error('ERROR: Invalid command');
@@ -35,8 +65,24 @@ function main(args) {
     }
 }
 
+function exec(command) {
+    return new Promise((resolve, reject) => {
+        console.log('Running:', command);
+        child_process.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(stderr);
+                reject(error);
+            }
+            else {
+                console.log(stdout);
+                resolve();
+            }
+        });
+    });
+}
+
 function usage() {
-    console.log('vuelab v' + package.version);
+    console.log('vuelab v' + pkg.version);
     console.log('Available commands:');
     console.log('vuelab init {{PROJECT_DIRECTORY}}');
     console.log('vuelab create route {{URL}}');
